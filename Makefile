b4550823448645da7ef219a988f1e246c1cdebc5
Kenrick Rilee <kenrick@Kenricks-MacBook-Pro.local>
2020-06-16 22:08:00 -0700

    call JS from C++ and C++ from JS

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
-.DEFAULT_GOAL := index.js
+all : output/index.js
 
-index.js : main.cpp
-	emcc main.cpp -std=c++11 -s WASM=1 -s USE_SDL=2 -O3 -o output/index.js
+output/index.js : main.cpp library.js
+	emcc main.cpp --js-library library.js -std=c++11 -s WASM=1 -s USE_SDL=2 -O3 -o output/index.js
+
+clean :
+	rm output/index.js
 
--- main.cpp
+++ main.cpp
@@ -1,102 +1,56 @@
 #include <functional>
-
+#include <stdlib.h>
 #include <emscripten.h>
 #include <SDL.h>
 
 #define GL_GLEXT_PROTOTYPES 1
 #include <SDL_opengles2.h>
 
-// Shader sources
-const GLchar* vertexSource =
-    "attribute vec4 position;                     \n"
-    "void main()                                  \n"
-    "{                                            \n"
-    "  gl_Position = vec4(position.xyz, 1.0);     \n"
-    "}                                            \n";
-const GLchar* fragmentSource =
-    "precision mediump float;\n"
-    "void main()                                  \n"
-    "{                                            \n"
-    "  gl_FragColor[0] = gl_FragCoord.x/640.0;    \n"
-    "  gl_FragColor[1] = gl_FragCoord.y/480.0;    \n"
-    "  gl_FragColor[2] = 0.5;                     \n"
-    "}                                            \n";
+///////////////////////////////////////////////////////////////////////////////
+// => C++
 
-// an example of something we will control from the javascript side
-bool background_is_black = true;
+extern "C" {
+  void EMSCRIPTEN_KEEPALIVE onClick() { printf("onClick\n"); }
 
-// the function called by the javascript code
-extern "C" void EMSCRIPTEN_KEEPALIVE toggle_background_color() { background_is_black = !background_is_black; }
+  EMSCRIPTEN_KEEPALIVE void* createBuffer(int bytes) {
+    return malloc(bytes * sizeof(char));
+  }
+
+  EMSCRIPTEN_KEEPALIVE void destroyBuffer(void* pointer) {
+    free(pointer);
+  }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// => JS
+
+extern "C" {
+  extern void testJS();
+}
+
+///////////////////////////////////////////////////////////////////////////////
 
 std::function<void()> loop;
-void main_loop() { loop(); }
+void mainLoop() {
+  loop();
+}
 
-int main()
-{
-    SDL_Window *window;
-    SDL_CreateWindowAndRenderer(640, 480, 0, &window, nullptr);
+int main() {
+  testJS();
 
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+  SDL_Window *window;
+  SDL_CreateWindowAndRenderer(640, 480, 0, &window, nullptr);
 
-    // Create a Vertex Buffer Object and copy the vertex data to it
-    GLuint vbo;
-    glGenBuffers(1, &vbo);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+  SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+  SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
 
-    GLfloat vertices[] = {0.0f, 0.5f, 0.5f, -0.5f, -0.5f, -0.5f};
+  loop = [&] {
+    SDL_GL_SwapWindow(window);
+  };
 
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  emscripten_set_main_loop(mainLoop, 0, true);
 
-    // Create and compile the vertex shader
-    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexSource, nullptr);
-    glCompileShader(vertexShader);
-
-    // Create and compile the fragment shader
-    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentSource, nullptr);
-    glCompileShader(fragmentShader);
-
-    // Link the vertex and fragment shader into a shader program
-    GLuint shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    glUseProgram(shaderProgram);
-
-    // Specify the layout of the vertex data
-    GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
-    glEnableVertexAttribArray(posAttrib);
-    glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
-
-    loop = [&]
-    {
-        static int i = 0;
-        i ++;
-        printf("%d\n", i);
-        // move a vertex
-        const uint32_t milliseconds_since_start = SDL_GetTicks();
-        const uint32_t milliseconds_per_loop = 3000;
-        vertices[0] = ( milliseconds_since_start % milliseconds_per_loop ) / float(milliseconds_per_loop) - 0.5f;
-        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-        // Clear the screen
-        if( background_is_black )
-            glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        else
-            glClearColor(0.9f, 0.9f, 0.9f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        // Draw a triangle from the 3 vertices
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
-        SDL_GL_SwapWindow(window);
-    };
-
-    emscripten_set_main_loop(main_loop, 0, true);
-
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
