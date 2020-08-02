524211cffe998a97dfb7c490e78fe9ee08022819
cricklet <cricklet@users.noreply.github.com>
2020-08-01 21:10:20 -0700

    .

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -53,7 +53,7 @@ int main() {
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
   glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-  GLFWwindow* window = glfwCreateWindow(640, 480, "OpenGL", nullptr, nullptr); // Windowed
+  GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", nullptr, nullptr); // Windowed
   glfwMakeContextCurrent(window);
 
   glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
