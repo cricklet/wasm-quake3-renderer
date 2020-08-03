e44fab9266954c904084ca3454c1b13d7b5cd676
cricklet <cricklet@users.noreply.github.com>
2020-08-03 11:11:25 -0700

    starting to load a specific page for osx

--- q-osx/q-osx/main.cpp
+++ q-osx/q-osx/main.cpp
@@ -1,6 +1,8 @@
 #include "support.h"
+#include "webview.h"
 
 int main(int argc, const char * argv[]) {
+  // Setup GL window
   glfwInit();
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
@@ -11,7 +13,16 @@ int main(int argc, const char * argv[]) {
   GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", nullptr, nullptr); // Windowed
   glfwMakeContextCurrent(window);
 
-  glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+  glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+  
+  
+  // Setup the webview
+  webview::webview w(true, nullptr);
+  w.set_title("q");
+  w.set_size(300, 600, WEBVIEW_HINT_FIXED);
+  w.navigate("http://0.0.0.0:8000/osx.html");
+  w.run();
+  
 
   printf("OpenGL version supported by this platform : %s\n", glGetString(GL_VERSION));
 
